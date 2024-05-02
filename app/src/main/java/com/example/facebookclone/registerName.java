773fd0b7976c2b4f23097d9c9e70dfeafcd87408
lanhthon <58631568+lanhthon@users.noreply.github.com>
2024-05-02 19:08:45 +0700

    Giao diện register

--- app/src/main/java/com/example/facebookclone/registerName.java
+++ app/src/main/java/com/example/facebookclone/registerName.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class registerName extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register_name);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button registerButton = findViewById(R.id.button_Register);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        Button nextButton = findViewById(R.id.button_conf_name);
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, registerName.class);
+                String ho = ((EditText)findViewById(R.id.editText_Ho)).getText().toString();
+                String ten = ((EditText)findViewById(R.id.editText_Ten)).getText().toString();
+
+                Intent intent = new Intent(registerName.this, registerDate.class);
+                intent.putExtra("HO", ho);
+                intent.putExtra("TEN", ten);
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/facebookclone/registerUser.java
+++ app/src/main/java/com/example/facebookclone/registerUser.java
@@ -1,9 +1,7 @@
 package com.example.facebookclone;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +9,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class registerUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register_user);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button registerButton = findViewById(R.id.button_Register);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, registerName.class);
-                startActivity(intent);
-            }
-        });
+        String ho = getIntent().getStringExtra("HO");
+        String ten = getIntent().getStringExtra("TEN");
+        String ngaySinh = getIntent().getStringExtra("NGAY_SINH");
+
+        // Hiển thị dữ liệu trên giao diện của registerUser
+        TextView hoTextView = findViewById(R.id.textView2);
+        TextView tenTextView = findViewById(R.id.textView3);
+        TextView ngaySinhTextView = findViewById(R.id.textView4);
+        hoTextView.setText("Họ: " + ho);
+        tenTextView.setText("Tên: " + ten);
+        ngaySinhTextView.setText("Ngày sinh: " + ngaySinh);
     }
 }
