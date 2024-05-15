1e7dd637ff5f9bfb3e27d2bb26a786e618a8ff59
lanhthon <58631568+lanhthon@users.noreply.github.com>
2024-05-16 00:22:02 +0700

    Revert "V1.0"

--- app/src/main/java/com/example/facebookclone/MainActivity.java
+++ app/src/main/java/com/example/facebookclone/MainActivity.java
@@ -1,130 +1,24 @@
 package com.example.facebookclone;
 
-import static com.example.facebookclone.R.layout.activity_main;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.facebookclone.fragmentPager.homeFragment;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonLogin;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(activity_main);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        mAuth = FirebaseAuth.getInstance();
-
-        editTextEmail = findViewById(R.id.editText_user);
-        editTextPassword = findViewById(R.id.editText_Password);
-        buttonLogin = findViewById(R.id.button_login);
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(".info/connected");
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolean connected = snapshot.getValue(Boolean.class);
-                if (connected) {
-                    // Kết nối được thiết lập
-                    Log.d("TAG", "Kết nối thành công với Realtime Database");
-                } else {
-                    // Mất kết nối
-                    Log.d("TAG", "Mất kết nối với Realtime Database");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // Xử lý khi có lỗi xảy ra
-                Log.w("TAG", "Lỗi khi lắng nghe kết nối với Realtime Database", error.toException());
-            }
-        });
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginUser();
-            }
-        });
-        // Check if user is already logged in
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            // Redirect to news activity
-            startActivity(new Intent(MainActivity.this, tableLayout.class));
-            finish(); // Optional: finish current activity to prevent going back to login screen
-        }
-
-        Button registerButton = findViewById(R.id.button_Register);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, registerName.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void loginUser() {
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        if (email.isEmpty()) {
-            editTextEmail.setError("Vui lòng nhập email");
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (password.isEmpty()) {
-            editTextPassword.setError("Vui lòng nhập mật khẩu");
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(MainActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-                        // Navigate to another activity or perform any other action after successful login
-                        // Redirect to news activity
-                        startActivity(new Intent(MainActivity.this, tableLayout.class));
-                        finish(); // Optional: finish current activity to prevent going back to login screen
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(MainActivity.this, "Đăng nhập thất bại! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
     }
 }
